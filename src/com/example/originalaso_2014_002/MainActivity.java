c6d82d9992a189004d97345126a44b3649a0f838
student <student@PC111S35.fgakusei.local>
2014-06-06 11:04:37 +0900

    aaaaaaaaaaaaaaaaaaaaaa

---
+++ src/com/example/originalaso_2014_002/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.originalaso_2014_002;
+
+import android.app.Activity;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+	/**
+		 * @author student
+		 *
+		 */
+	public class MySQLiteOpenHelper extends SQLiteOpenHelper {
+
+		/* (非 Javadoc)
+		 * @see android.database.sqlite.SQLiteOpenHelper#onCreate(android.database.sqlite.SQLiteDatabase)
+		 */
+		@Override
+		public void onCreate(SQLiteDatabase db) {
+			// TODO 自動生成されたメソッド・スタブ
+
+		}
+
+		/* (非 Javadoc)
+		 * @see android.database.sqlite.SQLiteOpenHelper#onUpgrade(android.database.sqlite.SQLiteDatabase, int, int)
+		 */
+		@Override
+		public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+			// TODO 自動生成されたメソッド・スタブ
+
+		}
+
+	}
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
---
+++ src/com/example/originalaso_2014_002/MySQLiteOpenHelper.java
@@ -0,0 +1,21 @@
+package com.example.originalaso_2014_002;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteOpenHelper
+
+public class MySQLiteOpenHelper extends SQLiteOpenHelper {
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
