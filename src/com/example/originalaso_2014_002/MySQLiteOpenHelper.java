24c43e1c3a3de83868570790bb51ef3fc4c0fb35
student <student@PC111S35.fgakusei.local>
2014-06-11 15:23:16 +0900

    1201756 立石晋平

--- src/com/example/originalaso_2014_002/MySQLiteOpenHelper.java
+++ src/com/example/originalaso_2014_002/MySQLiteOpenHelper.java
@@ -1,21 +1,68 @@
 package com.example.originalaso_2014_002;
 
+import android.content.Context;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteCursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.database.sqlite.SQLiteOpenHelper
-
+import android.util.Log;
 public class MySQLiteOpenHelper extends SQLiteOpenHelper {
+
+	@Override
+	public MySQLiteOpenHelper(Context context) {
+
+		super(context,"20140021201756.sqlite3", null,1);
+
+	}
 
 	@Override
 	public void onCreate(SQLiteDatabase db) {
 		// TODO 自動生成されたメソッド・スタブ
+		db.execSQL("CREATE TABLE IF NOT EXISTS" +
+				"Hitokoto ( _id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL , phrase TEXT)");
 
 	}
 
 	@Override
-	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-		// TODO 自動生成されたメソッド・スタブ
-
+	public void onUpgrade(SQLiteDatabase db, int oldVersion){
+		db.execSQL("drop table Hitokoto;");
+		onCreate(db);
 	}
 
+	public void insertHitokoto(SQLiteDatabase db, String inputMsg){
+		try{
+			db.beginTransaction();
+			db.execSQL(sqlstr);
+			db.setTransectionSuccessful();
+		} catch (SQLException e) {
+			Log.e("ERROR", e.toString());
+
+		}finally {
+			db.endTransaction();
+		}
+
+		return;
+
+	}
+	public String selectRandomHitokoto(SQLiteDatabase db){
+
+		String rtString = null;
+
+		String sqlstr = "SELECT _id, phrase FROM Hitokoto ORDER BY RANDOM();";
+			try {
+				SQLiteCursor cursor= (SQLiteCursor)db.rawQuery(sqlstr, null);
+				if(cursor.getCount() !=0);{
+					cursor.moveToFirst();
+					rtString = cursor.getString(1);
+				}
+				cursor.close();
+			} catch (SQLException e){
+				Log.e("ERROR", e.toString());
+			}finally{
+
+			}
+
+	return rtString;
+
+	}
 }
